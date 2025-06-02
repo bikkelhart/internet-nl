a3f05dbbd43c5d897b22157decf1804fbb0b1a7c
Menno ter Laak <menno@bikkelhart.com>
2025-06-02 18:03:27 +0200

    feat: add legacy js bundle with nomodule as check

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -16,53 +16,68 @@ const cleanupOutputDir = () => ({
   },
 });
 
+const defaultPlugins = [
+  isProduction &&
+    terser({
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: false,
+      },
+    }),
+];
+
+// Define chunks configuration
+const chunks = {
+  base: [
+    "src/js/base/header.js",
+    "src/js/base/theme.js",
+    "src/js/base/language-switch.js",
+    "src/js/base/detect-browser-font-size.js",
+    "src/js/base/initial.js",
+    "src/js/base/print.js",
+    "src/js/lib/matomo.js",
+  ],
+  results: [
+    "src/js/pages/results/copy-link.js",
+    "src/js/pages/results/repeat-test-countdown.js",
+  ],
+  connection: ["src/js/pages/connection/connection.js"],
+  probe: ["src/js/pages/probe/probe.js"],
+  "components/card-list": ["src/js/components/card-list/load-more.js"],
+  "components/carousel": ["src/js/components/carousel/carrousel.js"],
+  "components/meter": ["src/js/components/meter/result-meter.js"],
+  "components/action-card": [
+    "src/js/components/action-card/action-card-fallback.js",
+  ],
+};
+
 // Create config for each file
 const config = [
-  // JavaScript bundle
+  // Modern JavaScript bundle (ES modules)
   {
     input: "src/index.js",
     output: {
       format: "es",
       sourcemap: !isProduction,
       dir: `${outputDir}/js`,
-      manualChunks: {
-        base: [
-          "src/js/base/header.js",
-          "src/js/base/theme.js",
-          "src/js/base/language-switch.js",
-          "src/js/base/detect-browser-font-size.js",
-          "src/js/base/initial.js",
-          "src/js/base/print.js",
-          "src/js/lib/matomo.js",
-        ],
-        results: [
-          "src/js/pages/results/copy-link.js",
-          "src/js/pages/results/repeat-test-countdown.js",
-        ],
-        connection: ["src/js/pages/connection/connection.js"],
-        probe: ["src/js/pages/probe/probe.js"],
-        "components/card-list": ["src/js/components/card-list/load-more.js"],
-        "components/carousel": ["src/js/components/carousel/carrousel.js"],
-        "components/meter": ["src/js/components/meter/result-meter.js"],
-        "components/action-card": [
-          "src/js/components/action-card/action-card-fallback.js",
-        ],
-      },
+      manualChunks: chunks,
       chunkFileNames: "[name]-min.js",
     },
-    plugins: [
-      cleanupOutputDir(),
-      isProduction &&
-        terser({
-          compress: {
-            drop_console: true,
-            drop_debugger: true,
-          },
-          format: {
-            comments: false,
-          },
-        }),
-    ],
+    plugins: [cleanupOutputDir(), ...defaultPlugins],
+  },
+  // Legacy JavaScript bundle (IIFE)
+  {
+    input: "src/index.js",
+    output: {
+      format: "iife",
+      sourcemap: !isProduction,
+      file: `${outputDir}/js/legacy-min.js`,
+      name: "internetnl",
+    },
+    plugins: defaultPlugins,
   },
   // CSS bundle
   {
