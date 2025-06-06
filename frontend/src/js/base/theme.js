678a3074bbefbc1e27dbab3718760626b002230d
Menno ter Laak <menno@bikkelhart.com>
2025-06-06 13:03:31 +0200

    refactor(frontend): move all theme related stuff to theme.js

--- frontend/src/js/base/theme.js
+++ frontend/src/js/base/theme.js
@@ -33,13 +33,14 @@ function getCookie(name) {
  * @param {number} days - Cookie duration in days
  */
 function setCookie(name, value, days) {
-  let expires = "";
-  if (days) {
-    const date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = `;expires=${date.toUTCString()}`;
-  }
-  document.cookie = `${name}=${value || ""}${expires};path=/`;
+  const date = new Date();
+  date.setDate(date.getDate() + days);
+
+  const cookieValue = encodeURIComponent(value);
+  const expires = `expires=${date.toUTCString()}`;
+  const path = "path=/";
+
+  document.cookie = `${name}=${cookieValue};${expires};${path}`;
 }
 
 /**
