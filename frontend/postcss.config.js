ff51c119f62f97644dbea33986d870c8d11f9677
Menno ter Laak <menno@bikkelhart.com>
2025-06-12 12:25:25 +0200

    build(frontend): add variable compress plugin for postcss

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,10 @@
 export default {
   plugins: {
     "postcss-import": {},
-    "@csstools/postcss-cascade-layers": {},
+    "@csstools/postcss-cascade-layers": {
+      onImportLayerRule: "warn",
+    },
+    "postcss-variable-compress": {},
     autoprefixer: {},
     "postcss-preset-env": {
       features: {},
